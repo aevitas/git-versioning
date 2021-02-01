5aab2e8c3534569bd9166abe208c69e12d976359
aevitas <m4rtijn@gmail.com>
2021-02-01 10:10:08 +0100

    Add some code and minver

---
+++ src/Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var n in Enumerable.Range(0, 100))
+                Console.WriteLine(n);
+
+            Console.ReadLine();
+        }
+    }
+}
